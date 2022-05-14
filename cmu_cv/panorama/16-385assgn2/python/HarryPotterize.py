9a4ca9ed0a624b3ca5bc219fffeec71cd836e828
Steve <41510131+EeToSe@users.noreply.github.com>
2022-05-14 21:30:19 +0800

    remove cmu 16-385 ass2

--- cmu_cv/panorama/16-385assgn2/python/HarryPotterize.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-import skimage.io 
-import skimage.color
-#Import necessary functions
-
-
-
-#Write script for Q3.9
--- cmu_cv/panorama/16-385assgn2/zip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -FSr soln2.zip . -x ".*" -x "__MACOSX" -x "*Icon*" -x "assgn2.pdf"
