f41b84e9eeef852bf3ed559dbd9913e71960414d
Steve <41510131+EeToSe@users.noreply.github.com>
2022-07-03 10:24:43 +0800

    update harris corner

---
+++ image_analysis/k-means.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2 as cv
+
+# Color Quantization with K-means
+img = cv.imread('logo.png')
+Z = img.reshape((-1,3))
+# convert to np.float32
+Z = np.float32(Z)
+# define criteria, number of clusters(K) and apply kmeans()
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+K = 2
+ret,label,center=cv.kmeans(Z,K,None,criteria,10,cv.KMEANS_RANDOM_CENTERS)
+# Now convert back into uint8, and make original image
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+cv.imwrite('segmented.png', res2)
+
